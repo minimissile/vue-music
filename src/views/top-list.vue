d4f92b1768ff90ee9a23571780169acdd1f2d960
minimissile <3528816081@qq.com>
2021-06-20 17:42:44 +0800

    1

---
+++ src/views/top-list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search">
+    top-list.vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top-list'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
